b75dadfb6ca99800703de5f4568f414e30b5df02
alirezatab <alirezatabrizi10@gmail.com>
2016-11-18 15:00:29 -0800

    Created the xCode project

---
+++ PushNotification/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PushNotification
+//
+//  Created by ALIREZA TABRIZI on 11/18/16.
+//  Copyright © 2016 AR-T.com, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ PushNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushNotification
+//
+//  Created by ALIREZA TABRIZI on 11/18/16.
+//  Copyright © 2016 AR-T.com, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
